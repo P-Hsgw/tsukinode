bd5aed0dd6272f3d0d91dc832168d0388b67d185
P-Hsgw <ravandil@gmail.com>
2021-05-07 10:00:32 +0900

    Update 404 error page'

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
 import * as React from "react"
 
+
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Card, Section, Box } from "../components/section"
+
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className="text-red-500"></h1>
+    <p></p>
+    <Section sectionColor="gray-50" sectionID="error">
+    <Box title="Error 404" firstParagraph="You just hit a route that doesn&#39;t exist... the sadness."/>
+    </Section>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react"
-import Hero from "../components/hero"
 import Seo from "../components/seo"
-import Form from "../components/form"
 
+import Hero from "../components/hero"
+import Form from "../components/form"
 import Layout from "../components/layout"
 import { Card, Section, Box } from "../components/section"
 
